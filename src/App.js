17fda380a7e71e2509e5f5832daa08f213d05fb4
sarah-lima <sarah.lima@bandtec.com.br>
2022-08-01 01:46:58 -0300

    ajuste em selct

--- src/App.js
+++ src/App.js
@@ -13,32 +13,41 @@ function App() {
   const [fileType, setFileType] = useState("EXCEL");
 
   useEffect(() => {
-      api.get().then(({ data }) => {
-        setRes(data.data);
-      });
+    api.get().then(({ data }) => {
+      setRes(data.data);
+    });
   }, []);
 
-  const submit = ()=>{
-    console.log(initialDate)
-    console.log(finallyDate)
-    console.log(fileType)
+  const submit = () => {
     var a = document.createElement("a");
     document.body.appendChild(a);
     a.style = "display: none";
-    api.post(`/?init_data=${initialDate}&terminal_data=${finallyDate}&file_type=${fileType}`)
-      .then(res=>{
+    api
+      .post(
+        `/?init_data=${initialDate}&terminal_data=${finallyDate}&file_type=${fileType}`
+      )
+      .then((res) => {
         a.href = res.request.responseURL;
-        a.download = res.request.responseURL.substring(res.request.responseURL.lastIndexOf("_")+1);
+        a.download = res.request.responseURL.substring(
+          res.request.responseURL.lastIndexOf("_") + 1
+        );
         a.click();
         window.URL.revokeObjectURL(res.request.responseURL);
-      }).catch(error=> console.log('Erro ao fazer download'))
-    
-  }
+      })
+      .catch((error) => console.log("Erro ao fazer download"));
+  };
 
   return (
     <>
       <Header />
-      <div style={{ marginLeft: "10%", marginTop: "35px", display:'flex', gap: '28px' }}>
+      <div
+        style={{
+          marginLeft: "10%",
+          marginTop: "35px",
+          display: "flex",
+          gap: "28px",
+        }}
+      >
         <Input
           onChange={(e) => setInitialDate(e.target.value)}
           placeholder="Insira a data inicial"
@@ -47,17 +56,20 @@ function App() {
           onChange={(e) => setFinallyDate(e.target.value)}
           placeholder="Insira a data final"
         />
-        <select onChange={(e)=>setFileType(e.target.value)}>
-          <option value={'EXCEL'}>
-            EXCEL
-          </option>
-          <option value={'JSON'}>
-            JSON
-          </option>
+        <select
+          style={{
+            width: "253px",
+            height: "40px",
+            background: "#FFFFFF",
+            border: "1px solid #CDCDCD",
+            borderRadius: "3px",
+          }}
+          onChange={(e) => setFileType(e.target.value)}
+        >
+          <option value={"EXCEL"}>EXCEL</option>
+          <option value={"JSON"}>JSON</option>
         </select>
-        <Button onClick={submit} >
-          Filtrar
-        </Button>
+        <Button onClick={submit}>Filtrar</Button>
       </div>
       {res.map((item) => {
         return <Card data={item} />;
--- src/components/button/style.js
+++ src/components/button/style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const ButtonComponent=styled.button`
 width: 92px;
 height: 40px;
-
+border: none;
 background: #BBE6E4;
 border-radius: 3px;
 `
